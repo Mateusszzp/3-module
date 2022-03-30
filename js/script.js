bd69608ef2152930260a289558544f7f69d0a6dc
Mateusz Paśko <mateusz.pasko.r@gmail.com>
2022-03-30 04:44:14 +0200

    added og:image and improved readability

--- js/script.js
+++ js/script.js
@@ -1,52 +1,44 @@
 {
     function welcome() {
         console.log("Witam wszystkich developerów");
-    }
-    welcome();
- 
-    const ShowTheResult = (field, currency) => {
+    };
+    const ShowTheResult = (userValue, currency) => {
         const Euro = 4.6940;
         const Dolar = 4.2375;
         const Funt = 5.5831;
- 
+
         switch (currency) {
             case "EUR":
-                return field / Euro;
- 
+                return userValue / Euro;
+
             case "USD":
-                return field / Dolar;
- 
+                return userValue / Dolar;
+
             case "GBP":
-                return field / Funt;
-        }
+                return userValue / Funt;
+        };
     };
- 
-    const updateResultText = (field, currency, result) => {
+    const updateResultText = (userValue, currency, result) => {
         const resultElement = document.querySelector(".js-result");
-        resultElement.innerText = `${field.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`
- 
-    }
+        resultElement.innerText = `${userValue.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`
+
+    };
     const onFormSubmit = (event) => {
         event.preventDefault();
- 
-        const fieldElement = document.querySelector(".js-field");
+
+        const userValueElement = document.querySelector(".js-userValue");
         const currencyElement = document.querySelector(".js-currency");
- 
- 
- 
-        const field = +fieldElement.value
+
+        const userValue = +userValueElement.value
         const currency = currencyElement.value
- 
-        const result = ShowTheResult(field, currency)
- 
-        updateResultText(field, currency, result)
+        const result = ShowTheResult(userValue, currency)
+        updateResultText(userValue, currency, result)
     };
- 
     const init = () => {
         const formElement = document.querySelector(".js-form");
- 
         formElement.addEventListener("submit", onFormSubmit);
+        welcome();
     };
     init();
- 
+
 }
